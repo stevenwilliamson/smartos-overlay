19c1310c18d7969a3351e56878af2278e481b9a5
Thomas Haggett <thomas@haggett.org>
2012-06-18 23:00:44 +0100

    Ditch the old prompt-config script, and add our own configure-zpool script, and zookeeper node.js agent.

---
+++ smartdc/bin/configuration-agent.js
@@ -0,0 +1,171 @@
+#!/usr/bin/node
+/*
+########################################################
+## ZooKeeper Configuration Agent
+##
+## Connects to a ZooKeeper server, announces the sysinfo 
+## data, then waits for configuration parameters to be 
+## written. 
+## When written, executes the configuration steps and
+## terminates.
+#######################################################
+*/
+
+var cp = require('child_process')
+var execString = cp.exec
+
+var fs = require('fs')
+var os = require('os')
+var ZooKeeper = require("zookeeper")
+
+if(process.argv.length != 4) {
+  console.log("Must be executed:");
+  console.log("  node "+__filename+" <zookeeper> <usb-root>")
+  process.exit(1)
+}
+
+// presume we're invoked with node
+//  node /some/script.js zoo1.local:2181,zoo2.local:2181
+var zookeeperAddress = process.argv[2]
+var zookeeperBase = "/hypervisors/unconfigured"
+
+// local provisioning steps
+var zpoolScript = "/smartdc/bin/freeagent-configure-zpool.sh";
+var configurationPath = process.argv[3] + "/config"
+
+// Connect to ZooKeeper
+var zk = null;
+var configurationStarted = false;
+
+function sysinfo(callback) {
+  execString('/usr/bin/sysinfo', function (error, stdout, stderr) {
+    var obj;
+    if (error) {
+      callback(new Error(stderr.toString()));
+    } else {
+      stdout_string = stdout.toString();
+      obj = JSON.parse(stdout_string);
+      callback(null, obj, stdout_string);
+    }
+  });
+}
+
+var provision = function(configuration) {
+  console.log("Provisioning:");
+  console.log("  executing: "+zpoolScript+" "+configuration["zpool_args"])
+
+  updateZookeeperStatus("Creating zpool");
+  execString(zpoolScript + " " + configuration["zpool_args"], function(error, stdout, stderr) {
+    console.log("stdout: "+stdout.toString())
+    console.log("stderr: "+stderr.toString())
+
+    if (error) {
+      console.log("Failed to create zpool: "+stderr.toString());
+      zk.close(function() { process.exit(1) });
+    } else {
+      console.log("zpool created.")
+      updateZookeeperStatus("Writing configuration");
+      fs.writeFile(configurationPath, configuration['config_file'], function(err) {
+        if(err) {
+          console.log("Failed to write file: " + err);
+          zk.close(function() { process.exit(1) });
+
+        } else {
+          console.log("Configuration written. We're done!");
+          zk.close(function() { process.exit(0) });
+        }
+      });
+    }
+  });
+};
+
+// handle the initial connection to zookeeper
+var zookeeperConnectReturned = function(err) {
+  if( err ) {
+    // Bah, we failed to connect.
+    console.log("Failed to connect to zookeeper: "+err.toString());
+    console.log("  retrying in 2 seconds...");
+
+    // Try again in a moment...
+    setTimeout(connectToZookeeper, 2000);
+  } else {
+    zookeeperConnected();
+  }
+}
+var connectToZookeeper = function() {
+  console.log("Connecting to ZooKeeper server at '" + zookeeperAddress + "'");
+
+  zk = new ZooKeeper({
+      connect: zookeeperAddress
+    , timeout: 2000
+    , debug_level: ZooKeeper.ZOO_LOG_LEVEL_WARNING
+    , host_order_deterministic: false
+  });
+
+  zk.connect(zookeeperConnectReturned);
+}
+var updateZookeeperStatus = function(msg) {
+
+};
+
+// this is called whenever we notice a new value for the znode we create.
+var handleConfigurationData = function(data) {
+  console.log("ZooKeeper data retrieved");
+
+  // parse the JSON struct
+  try {
+    data = JSON.parse(data);
+  } catch(e) {
+    console.log("Failed to parse JSON: "+e.toString());
+    return;
+  }
+
+  // do we have a configuration set yet?
+  if('provisioning_configuration' in data) {
+    if(configurationStarted == true) {
+      console.log("Configuration already underway...");
+      return;
+    }
+    configurationStarted = true;
+    provision(data['provisioning_configuration']);
+  } else {
+    console.log("No provisioning config set...");
+  }
+}
+
+// Zookeeper events
+var zookeeperConnected = function() {
+
+  console.log("We're connected!");
+
+  zk.mkdirp(zookeeperBase, function(error) {
+    if(error) throw error;
+
+    // grab the sysinfo for this node
+    sysinfo(function(error, info, string) {
+      var nodeContents = {
+        'sysinfo': info
+      }
+
+      // write the sysinfo to a node, and keep an eye on it !
+      zk.a_create(zookeeperBase + "/smartos-", JSON.stringify(nodeContents), ZooKeeper.ZOO_SEQUENCE | ZooKeeper.ZOO_EPHEMERAL, function(rc, error, config_path) { 
+        
+        console.log("Node has been created: "+config_path);
+
+        // the initial get
+        zk.a_get(config_path, true, function(rc, error, stat, value) { handleConfigurationData(value); });
+        
+        // keep watching a node!
+        zk.on(ZooKeeper.on_event_changed, function( zkk, path ) {
+
+          // keep watching
+          zk.a_get(config_path, true, function(rc, error, stat, value) { handleConfigurationData(value); });
+        }); //zk.on(ZooKeeper..)
+      }); //zk.a_create(zookeeperBase)
+    }); // sysinfo
+  }); //zk.mkdirp
+}
+
+// kick it all off!
+connectToZookeeper();
+
---
+++ smartdc/bin/freeagent-configure-zpool.sh
@@ -0,0 +1,206 @@
+#!/usr/bin/bash
+
+PATH=/usr/sbin:/usr/bin
+export PATH
+. /lib/sdc/config.sh
+load_sdc_sysinfo
+load_sdc_config
+
+sigexit()
+{
+  echo
+  echo "System configuration has not been completed."
+  echo "You must reboot to re-run system configuration."
+  exit 0
+}
+
+create_dump()
+{
+    # Get avail zpool size - this assumes we're not using any space yet.
+    base_size=`zfs get -H -p -o value available ${SYS_ZPOOL}`
+    # Convert to MB
+    base_size=`expr $base_size / 1000000`
+    # Calculate 5% of that
+    base_size=`expr $base_size / 20`
+    # Cap it at 4GB
+    [ ${base_size} -gt 4096 ] && base_size=4096
+
+    # Create the dump zvol
+    zfs create -V ${base_size}mb ${SYS_ZPOOL}/dump || \
+      fatal "failed to create the dump zvol"
+    dumpadm -d /dev/zvol/dsk/${SYS_ZPOOL}/dump
+}
+
+#
+# Setup the persistent datasets on the zpool.
+#
+setup_datasets()
+{
+  datasets=$(zfs list -H -o name | xargs)
+  
+  if ! echo $datasets | grep dump > /dev/null; then
+    printf "%-56s" "Making dump zvol... " 
+    create_dump
+    printf "%4s\n" "done" 
+  fi
+
+  if ! echo $datasets | grep ${CONFDS} > /dev/null; then
+    printf "%-56s" "Initializing config dataset for zones... " 
+    zfs create ${CONFDS} || fatal "failed to create the config dataset"
+    chmod 755 /${CONFDS}
+    cp -p /etc/zones/* /${CONFDS}
+    zfs set mountpoint=legacy ${CONFDS}
+    printf "%4s\n" "done" 
+  fi
+
+  if ! echo $datasets | grep ${USBKEYDS} > /dev/null; then
+    if [[ -n $(/bin/bootparams | grep "^smartos=true") ]]; then
+        printf "%-56s" "Creating config dataset... " 
+        zfs create -o mountpoint=legacy ${USBKEYDS} || \
+          fatal "failed to create the config dataset"
+        mkdir /usbkey
+        mount -F zfs ${USBKEYDS} /usbkey
+        printf "%4s\n" "done" 
+    fi
+  fi
+
+  if ! echo $datasets | grep ${COREDS} > /dev/null; then
+    printf "%-56s" "Creating global cores dataset... " 
+    zfs create -o quota=10g -o mountpoint=/${SYS_ZPOOL}/global/cores \
+        -o compression=gzip ${COREDS} || \
+        fatal "failed to create the cores dataset"
+    printf "%4s\n" "done" 
+  fi
+
+  if ! echo $datasets | grep ${OPTDS} > /dev/null; then
+    printf "%-56s" "Creating opt dataset... " 
+    zfs create -o mountpoint=legacy ${OPTDS} || \
+      fatal "failed to create the opt dataset"
+    printf "%4s\n" "done" 
+  fi
+
+  if ! echo $datasets | grep ${VARDS} > /dev/null; then
+    printf "%-56s" "Initializing var dataset... " 
+    zfs create ${VARDS} || \
+      fatal "failed to create the var dataset"
+    chmod 755 /${VARDS}
+    cd /var
+    if ( ! find . -print | cpio -pdm /${VARDS} 2>/dev/null ); then
+        fatal "failed to initialize the var directory"
+    fi
+
+    zfs set mountpoint=legacy ${VARDS}
+
+    if ! echo $datasets | grep ${SWAPVOL} > /dev/null; then
+          printf "%-56s" "Creating swap zvol... " 
+          #
+          # We cannot allow the swap size to be less than the size of DRAM, lest$
+          # we run into the availrmem double accounting issue for locked$
+          # anonymous memory that is backed by in-memory swap (which will$
+          # severely and artificially limit VM tenancy).  We will therfore not$
+          # create a swap device smaller than DRAM -- but we still allow for the$
+          # configuration variable to account for actual consumed space by using$
+          # it to set the refreservation on the swap volume if/when the$
+          # specified size is smaller than DRAM.$
+          #
+          size=${SYSINFO_MiB_of_Memory}
+          zfs create -V ${size}mb ${SWAPVOL}
+          swap -a /dev/zvol/dsk/${SWAPVOL}
+    fi
+    printf "%4s\n" "done" 
+  fi
+}
+
+
+create_zpool()
+{
+    disks=$1
+    pool=zones
+
+    # If the pool already exists, don't create it again.
+    if /usr/sbin/zpool list -H -o name $pool; then
+        return 0
+    fi
+
+    disk_count=$(echo "${disks}" | wc -w | tr -d ' ')
+    printf "%-56s" "Creating pool $pool... " 
+
+    # If no pool profile was provided, use a default based on the number of
+    # devices in that pool.
+    if [[ -z ${profile} ]]; then
+        case ${disk_count} in
+        0)
+             fatal "no disks found, can't create zpool";;
+        1)
+             profile="";;
+        2)
+             profile=mirror;;
+        *)
+             profile=raidz;;
+        esac
+    fi
+
+    zpool_args=""
+
+    # When creating a mirrored pool, create a mirrored pair of devices out of
+    # every two disks.
+    if [[ ${profile} == "mirror" ]]; then
+        ii=0
+        for disk in ${disks}; do
+            if [[ $(( $ii % 2 )) -eq 0 ]]; then
+                  zpool_args="${zpool_args} ${profile}"
+            fi
+            zpool_args="${zpool_args} ${disk}"
+            ii=$(($ii + 1))
+        done
+    else
+        zpool_args="${profile} ${disks}"
+    fi
+
+    zpool create -f ${pool} ${zpool_args} || \
+        fatal "failed to create pool ${pool}"
+    zfs set atime=off ${pool} || \
+        fatal "failed to set atime=off for pool ${pool}"
+
+    printf "%4s\n" "done" 
+}
+
+create_zpools()
+{
+  devs=$1
+
+  export SYS_ZPOOL="zones"
+  create_zpool "$devs"
+  sleep 5
+
+  svccfg -s svc:/system/smartdc/init setprop config/zpool="zones"
+  svccfg -s svc:/system/smartdc/init:default refresh
+
+  export CONFDS=${SYS_ZPOOL}/config
+  export COREDS=${SYS_ZPOOL}/cores
+  export OPTDS=${SYS_ZPOOL}/opt
+  export VARDS=${SYS_ZPOOL}/var
+  export USBKEYDS=${SYS_ZPOOL}/usbkey
+  export SWAPVOL=${SYS_ZPOOL}/swap
+  
+  setup_datasets
+  #
+  # Since there may be more than one storage pool on the system, put a
+  # file with a certain name in the actual "system" pool.
+  #
+  touch /${SYS_ZPOOL}/.system_pool
+}
+
+trap sigexit SIGINT
+
+export TERM=sun-color
+export TERM=xterm-color
+stty erase ^H
+
+create_zpools $*
+
+clear
+
+cp -rp /etc/ssh /usbkey/ssh
+cp /usr/ds/etc/sources.list.sample /var/db/dsadm/sources.list
+
